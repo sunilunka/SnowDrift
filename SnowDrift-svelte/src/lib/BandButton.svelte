8540514387d6d5f4cddc7138c9102b3b0a5710e3
Sunil Unka <sunil.unka@gmail.com>
2023-06-02 01:05:52 +1200

    Working on initial prototype store logic.

---
+++ SnowDrift-svelte/src/lib/BandButton.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  type band = {
+    channel: number;
+    selected: boolean;
+  }
+  let bands: band[] = [{
+    channel: 1,
+    selected: false
+  }, {
+    channel: 2,
+    selected: false
+  }];
+
+  function toggleBand(evt): void {
+
+  }
+</script>
+
+{#each bands as band (band.channel)}
+<button on:click={toggleBand}>
+  {band.channel}
+</button>
+{/each}
+
+<style>
+  button {
+    border: 1px solid #00ff1e;
+  }
+</style>
